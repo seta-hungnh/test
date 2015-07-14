a3bb5c4ca9da49370adfc4ed9f66f92b9ca74286
seta-hungnh <hung.nguyen@setacinq.vn>
2015-07-14 11:05:49 +0700

    Bổ sung file templateconfig.php

---
+++ templateconfig.php
@@ -0,0 +1,224 @@
+<?php
+$templatenum = "3";
+$templateswitching = "1";
+$tmp['t1_mainimage'] = "img/home-photo.jpg";
+$tmp['t1_maintitle'] = "Our Family History";
+$tmp['t1_titleimage'] = "img/home-title.gif";
+$tmp['t1_headimgplustitle'] = "img/header-title.gif";
+$tmp['t2_mainimage'] = "img/mainphoto.jpg";
+$tmp['t2_maintitle'] = "Our Family<br />Genealogy Pages";
+$tmp['t2_titleimage'] = "img/homeheader.gif";
+$tmp['t2_pedperson'] = "I1";
+$tmp['t2_pedtree'] = "mytreeid";
+$tmp['t2_phhistperson'] = "I1";
+$tmp['t2_phhisttree'] = "mytreeid";
+$tmp['t2_headimg'] = "img/headerphoto.jpg";
+$tmp['t2_headtitleimg'] = "img/headertitle.gif";
+$tmp['t3_mainimage'] = "img/mainstreet.jpg";
+$tmp['t3_maintitle'] = "Our Family History";
+$tmp['t3_titleimage'] = "img/title.gif";
+$tmp['t3_headimg'] = "img/headerphoto.jpg";
+$tmp['t3_headtitleimg'] = "img/headertitle.gif";
+$tmp['t4_mainimage'] = "img/bigphoto.jpg";
+$tmp['t4_photocaption'] = "Mt. Pleasant, Utah, about 1915";
+$tmp['t4_headline'] = "Main Feature";
+$tmp['t4_mainpara'] = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros
+et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit a";
+$tmp['t4_featuretitle1'] = "Feature Story 1";
+$tmp['t4_featurelink1'] = "histories/feature1.php";
+$tmp['t4_featurethumb1'] = "img/featurethumb1.gif";
+$tmp['t4_featurepara1'] = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.";
+$tmp['t4_featuretitle2'] = "Feature Story 2";
+$tmp['t4_featurelink2'] = "histories/feature2.php";
+$tmp['t4_featurethumb2'] = "img/featurethumb2.gif";
+$tmp['t4_featurepara2'] = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.";
+$tmp['t4_featuretitle3'] = "Feature Story 3";
+$tmp['t4_featurelink3'] = "histories/feature3.php";
+$tmp['t4_featurethumb3'] = "img/featurethumb3.gif";
+$tmp['t4_featurepara3'] = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.";
+$tmp['t4_featuretitle4'] = "Feature Story 4";
+$tmp['t4_featurelink4'] = "histories/feature4.php";
+$tmp['t4_featurethumb4'] = "img/featurethumb1.gif";
+$tmp['t4_featurepara4'] = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.";
+$tmp['t4_headimg'] = "img/smallphoto.jpg";
+$tmp['t4_titleimg'] = "img/title.gif";
+$tmp['t5_maintitle'] = "Our Family History and Ancestry";
+$tmp['t5_mainimage'] = "img/mediumphoto.jpg";
+$tmp['t5_headimg'] = "img/smallphoto.jpg";
+$tmp['t5_mainpara'] = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud
+exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea
+commodo consequat. Duis autem vel eum iriure dolor in hendrerit in
+vulputate velit esse molestie consequat, vel illum dolore eu
+feugiat nulla facilisis at vero eros et accumsan et iusto odio
+dignissim qui blandit praesent luptatum zzril delenit a.";
+$tmp['t6_maintitle'] = "Our Family History";
+$tmp['t6_headimg'] = "img/titlebottom.jpg";
+$tmp['t6_mainpara'] = "This is where you can welcome the user to your site.";
+$tmp['t6_searchpara'] = "To search the genealogy database, enter a name below. You will find many families!";
+$tmp['t6_fhpara'] = "You can use this section to link to some of your more interesting family histories.";
+$tmp['t6_hisside'] = "His Side";
+$tmp['t6_herside'] = "Her Side";
+$tmp['t6_fhlinkshis'] = "<a href=\"extrastree.php?personID=I1&amp;tree=T0001&amp;showall=1\">Individual 1</a><br/>
+<a href=\"extrastree.php?personID=I2&amp;tree=T0001&amp;showall=1\">Individual 2</a><br/>";
+$tmp['t6_fhlinkshers'] = "<a href=\"extrastree.php?personID=I3&amp;tree=T0001&amp;showall=1\">Individual 3</a><br/>
+<a href=\"extrastree.php?personID=I4&amp;tree=T0001&amp;showall=1\">Individual 4</a><br/>";
+$tmp['t6_mwpara'] = "Use this section to display information about individuals you are actively researching.";
+$tmp['t6_respara'] = "List resources here that you would like to share with others.";
+$tmp['t6_reslinks'] = "<a href=\"http://www.ancestry.com\" target=\"_blank\">Ancestry.com</a><br />
+<a href=\"http://www.fold3.com\" target=\"_blank\">Fold3.com</a><br />
+<a href=\"http://lythgoes.net/genealogy/software.php\" target=\"_blank\">TNG Software</a>";
+$tmp['t7_mainimage'] = "img/bigphoto.jpg";
+$tmp['t7_maintitle'] = "Our Family<br />History";
+$tmp['t7_photocaption'] = "Mt. Pleasant, Utah, about 1915";
+$tmp['t7_headline'] = "Main Feature";
+$tmp['t7_mainpara'] = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros
+et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit a";
+$tmp['t7_titleimg'] = "img/logo.jpg";
+$tmp['t7_newstext'] = "This section can be used for brief news announcements";
+$tmp['t8_headimg'] = "img/titletop.jpg";
+$tmp['t8_mainpara'] = "This is where you can welcome a user to your site.  You might also give them some basic information on navigating and links to any help or FAQ pages you may have.";
+$tmp['t8_latestnews'] = "<b>5 Jan 2009</b> - Added a whole new look to my site.<br/><br/>
+<b>25 Dec 2008</b> - Put years and years of work into my family research.<br/><br/>";
+$tmp['t8_headtitle1'] = "surname";
+$tmp['t8_headtitle2'] = "genealogy";
+$tmp['t8_headtitle3'] = "pages";
+$tmp['t8_headsubtitle'] = "genealogy of the surname family";
+$tmp['t8_momside'] = "Mom's Side";
+$tmp['t8_momsidenames'] = "Mom's Side";
+$tmp['t8_momperson'] = "I1";
+$tmp['t8_momtree'] = "mytreeid";
+$tmp['t8_dadside'] = "Dad's Side";
+$tmp['t8_dadsidenames'] = "Dad's Side";
+$tmp['t8_dadperson'] = "I2";
+$tmp['t8_dadtree'] = "mytreeid";
+$tmp['t9_maintitle'] = "Our Family History";
+$tmp['t9_headsubtitle'] = "genealogy of the surname family";
+$tmp['t9_headimg'] = "img/header-object.png";
+$tmp['t9_welcome'] = "Welcome!";
+$tmp['t9_mainimage'] = "img/annie-age17-recolored.jpg";
+$tmp['t9_dadside'] = "His Side";
+$tmp['t9_dadperson'] = "I2";
+$tmp['t9_dadtree'] = "mytreeid";
+$tmp['t9_momside'] = "Her Side";
+$tmp['t9_momperson'] = "I1";
+$tmp['t9_momtree'] = "mytreeid";
+$tmp['t9_mainpara'] = "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra, tellus sit amet congue vulputate, nisi erat iaculis nibh, vitae feugiat sapien ante eget mauris. Cras elit nisl, rhoncus nec iaculis ultricies, feugiat eget sapien. Pellentesque ac felis tellus.</p>
+<p>Aenean sollicitudin imperdiet arcu, vitae dignissim est posuere id. Duis placerat justo eu nunc interdum ultrices. Phasellus elit dolor, porttitor id consectetur sit amet, posuere id magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+<p>Suspendisse pharetra auctor pharetra. Nunc a sollicitudin est. Curabitur ullamcorper gravida felis, sit amet scelerisque lorem iaculis sed. Donec vel neque in neque porta venenatis sed sit amet lectus. Fusce ornare elit nisl, feugiat bibendum lorem. Vivamus pretium dictum sem vel laoreet. In fringilla pharetra purus, semper vulputate ligula cursus in. Donec at nunc nec dui laoreet porta eu eu ipsum. Sed eget lacus sit amet risus elementum dictum.</p>";
+$tmp['t9_featurelinks'] = "Feature 1, showmedia.php?mediaID=xxx
+Feature 2, histories/feature2.php
+Feature 3, http://www.other-web-site.com/feature3.html";
+$tmp['t9_featurepara'] = "Lorem ipsum dolor sit amet. Nam sit amet sem. Mauris a ante.";
+$tmp['t9_reslinks'] = "Ancestry, http://www.ancestry.com
+Archives.com, http://www.archives.com
+Cyndi's List, http://cyndislist.com
+FamilySearch, http://www.familysearch.org
+Find a Grave, http://www.findagrave.com
+Fold3, http://www.fold3.com
+Rootsweb, http://www.rootsweb.com
+TNG, http://www.tngsitebuilding.com";
+$tmp['t10_maintitle'] = "Our Family History";
+$tmp['t10_headsubtitle'] = "genealogy of the surname family";
+$tmp['t10_headimg'] = "img/header-object.png";
+$tmp['t10_welcome'] = "Welcome!";
+$tmp['t10_mainimage'] = "img/beatrice-carlisle-soft.jpg";
+$tmp['t10_dadside'] = "His Side";
+$tmp['t10_dadperson'] = "I2";
+$tmp['t10_dadtree'] = "mytreeid";
+$tmp['t10_momside'] = "Her Side";
+$tmp['t10_momperson'] = "I1";
+$tmp['t10_momtree'] = "mytreeid";
+$tmp['t10_mainpara'] = "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra, tellus sit amet congue vulputate, nisi erat iaculis nibh, vitae feugiat sapien ante eget mauris. Cras elit nisl, rhoncus nec iaculis ultricies, feugiat eget sapien. Pellentesque ac felis tellus.</p>
+<p>Aenean sollicitudin imperdiet arcu, vitae dignissim est posuere id. Duis placerat justo eu nunc interdum ultrices. Phasellus elit dolor, porttitor id consectetur sit amet, posuere id magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+<p>Suspendisse pharetra auctor pharetra. Nunc a sollicitudin est. Curabitur ullamcorper gravida felis, sit amet scelerisque lorem iaculis sed. Donec vel neque in neque porta venenatis sed sit amet lectus. Fusce ornare elit nisl, feugiat bibendum lorem. Vivamus pretium dictum sem vel laoreet. In fringilla pharetra purus, semper vulputate ligula cursus in. Donec at nunc nec dui laoreet porta eu eu ipsum. Sed eget lacus sit amet risus elementum dictum.</p>";
+$tmp['t10_featurelinks'] = "Feature 1, showmedia.php?mediaID=xxx
+Feature 2, histories/feature2.php
+Feature 3, http://www.other-web-site.com/feature3.html";
+$tmp['t10_featurepara'] = "Lorem ipsum dolor sit amet. Nam sit amet sem. Mauris a ante.";
+$tmp['t10_reslinks'] = "Ancestry, http://www.ancestry.com
+Archives.com, http://www.archives.com
+Cyndi's List, http://cyndislist.com
+FamilySearch, http://www.familysearch.org
+Find a Grave, http://www.findagrave.com
+Fold3, http://www.fold3.com
+Rootsweb, http://www.rootsweb.com
+TNG, http://www.tngsitebuilding.com";
+$tmp['t11_maintitle'] = "Our Family History";
+$tmp['t11_mainimage'] = "img/mainphoto.jpg";
+$tmp['t11_dadside'] = "His Side";
+$tmp['t11_dadperson'] = "I2";
+$tmp['t11_dadtree'] = "mytreeid";
+$tmp['t11_momside'] = "Her Side";
+$tmp['t11_momperson'] = "I1";
+$tmp['t11_momtree'] = "mytreeid";
+$tmp['t11_mainpara'] = "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra, tellus sit amet congue vulputate, nisi erat iaculis nibh,
+vitae feugiat sapien ante eget mauris. Pellentesque ac felis tellus. Aenean sollicitudin imperdiet arcu, vitae dignissim est posuere id.
+Duis placerat justo eu nunc interdum ultrices. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra, tellus sit amet
+congue vulputate, nisi erat iaculis nibh, vitae feugiat sapien ante eget mauris. Pellentesque ac felis tellus. Aenean sollicitudin 
+imperdiet arcu, vitae dignissim est posuere id. Duis placerat justo eu nunc interdum ultrices.</p>";
+$tmp['t1_titlechoice'] = "image";
+$tmp['t2_titlechoice'] = "image";
+$tmp['t3_titlechoice'] = "text";
+$tmp['t4_titlechoice'] = "image";
+$tmp['t4_headtitle1'] = "Our Family";
+$tmp['t4_headtitle2'] = "Genealogy Pages";
+$tmp['t7_titlechoice'] = "image";
+$tmp['t8_featurespara'] = "<p><a href=\"mostwanted.php\">Most Wanted</a> - I am looking for information about these ancestors.  Can you help?</p>
+	<p><a href=\"whatsnew.php\">What's New</a> - View the latest updates made to the site.</p>";
+$tmp['t8_menutitle'] = "Site Menu";
+$tmp['t12_maintitle'] = "Our Family Genealogy Pages";
+$tmp['t12_headsubtitle'] = "Discovering our American, Canadian and European Ancestors";
+$tmp['t12_headimg'] = "img/book.png";
+$tmp['t12_welcome'] = "Welcome!";
+$tmp['t12_mainpara'] = "<p>This is where you can welcome a user to your site. You might also give them some basic information on navigating and links to any help or FAQ pages you may have.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra, tellus sit amet congue vulputate, nisi erat iaculis nibh, vitae feugiat sapien ante eget mauris. Cras elit nisl, rhoncus nec iaculis ultricies, feugiat eget sapien. Pellentesque ac felis tellus.</p>
+<p>Aenean sollicitudin imperdiet arcu, vitae dignissim est posuere id. Duis placerat justo eu nunc interdum ultrices. Phasellus elit dolor, porttitor id consectetur sit amet, posuere id magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+<p>Suspendisse pharetra auctor pharetra. Nunc a sollicitudin est. Curabitur ullamcorper gravida felis, sit amet scelerisque lorem iaculis sed. Donec vel neque in neque porta venenatis sed sit amet lectus. Fusce ornare elit nisl, feugiat bibendum lorem. Vivamus pretium dictum sem vel laoreet. In fringilla pharetra purus, semper vulputate ligula cursus in. Donec at nunc nec dui laoreet porta eu eu ipsum. Sed eget lacus sit amet risus elementum dictum.</p>";
+$tmp['t12_photol'] = "img/charlemagne1.jpg";
+$tmp['t12_phototitlel'] = "Emperor Charlemagne";
+$tmp['t12_photocaptionl'] = "King Charles I of France (Charlemagne) is a grandfather to many persons in our family tree. As a result we have many kings, queens, dukes, duchesses, etc. as grandparents and cousins.";
+$tmp['t12_featurelink1'] = "http://en.wikipedia.org/wiki/Charlemagne";
+$tmp['t12_photor'] = "img/augustinegrignon.jpg";
+$tmp['t12_phototitler'] = "Augustin Grignon";
+$tmp['t12_photocaptionr'] = "Augustin Grignon was a fur trader and general entrepreneur in the Fox River Valley in territorial Wisconsin, surviving into its early years of statehood.";
+$tmp['t12_featurelink2'] = "http://en.wikipedia.org/wiki/Augustin_Grignon";
+$tmp['t12_topsurnames'] = "Top 100 Surnames in Our Family Tree";
+$tmp['t13_maintitle'] = "Our Family History";
+$tmp['t13_mainimage'] = "img/mainphoto.jpg";
+$tmp['t13_dadside'] = "His Side";
+$tmp['t13_dadperson'] = "I2";
+$tmp['t13_dadtree'] = "mytreeid";
+$tmp['t13_momside'] = "Her Side";
+$tmp['t13_momperson'] = "I1";
+$tmp['t13_momtree'] = "mytreeid";
+$tmp['t13_mainpara'] = "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra, tellus sit amet congue vulputate, nisi erat iaculis nibh,
+vitae feugiat sapien ante eget mauris. Pellentesque ac felis tellus. Aenean sollicitudin imperdiet arcu, vitae dignissim est posuere id.
+Duis placerat justo eu nunc interdum ultrices. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra, tellus sit amet
+congue vulputate, nisi erat iaculis nibh, vitae feugiat sapien ante eget mauris. Pellentesque ac felis tellus. Aenean sollicitudin
+imperdiet arcu, vitae dignissim est posuere id. Duis placerat justo eu nunc interdum ultrices.</p>";
+$tmp['t14_maintitle'] = "Our Family History";
+$tmp['t14_headsubtitle'] = "The Genealogy of the Surname Family";
+$tmp['t14_mainimage'] = "img/mainphoto.jpg";
+$tmp['t14_photocaption'] = "Wedding photo for Niels Jensen and Nicolena Rolfsen, about 1852.";
+$tmp['t14_headimg'] = "img/smallphoto.jpg";
+$tmp['t14_headimg2'] = "img/smallphoto2.jpg";
+$tmp['t14_headimg3'] = "img/smallphoto3.jpg";
+$tmp['t14_dadside'] = "His Side";
+$tmp['t14_dadperson'] = "I2";
+$tmp['t14_dadtree'] = "mytreeid";
+$tmp['t14_momside'] = "Her Side";
+$tmp['t14_momperson'] = "I1";
+$tmp['t14_momtree'] = "mytreeid";
+$tmp['t14_welcome'] = "Welcome!";
+$tmp['t14_mainpara'] = "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra, tellus sit amet congue vulputate, nisi erat iaculis nibh,
+vitae feugiat sapien ante eget mauris. Pellentesque ac felis tellus. Aenean sollicitudin imperdiet arcu, vitae dignissim est posuere id.
+Duis placerat justo eu nunc interdum ultrices. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra, tellus sit amet
+congue vulputate, nisi erat iaculis nibh, vitae feugiat sapien ante eget mauris. Pellentesque ac felis tellus. Aenean sollicitudin
+imperdiet arcu, vitae dignissim est posuere id. Duis placerat justo eu nunc interdum ultrices.</p>";
+?>
