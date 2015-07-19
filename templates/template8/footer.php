1739f8c6976e91ca2f2d87018f62ff00cbab8dc3
seta-int08 <IgtqxKO7cBx0pors4n5h>
2015-07-19 15:13:13 +0700

    Bổ sung template

---
+++ templates/template8/footer.php
@@ -0,0 +1,13 @@
+<!-- begin footer -->
+<?php global $text, $tng_version; ?>
+	</td></tr>
+	<tr>
+	    <td colspan="2" class="footer">
+			<?php
+				$flags['basicfooter'] = true;
+				echo tng_footer($flags);
+			?>
+	    </td>
+	</tr>
+</table>
+<!-- end footer -->
