e172880aee5b73fba7bec2243f4cbe6c01feecf6
seta-int08 <IgtqxKO7cBx0pors4n5h>
2015-07-19 11:07:00 +0700

    Thay đổi tên công ty

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
-$tng_title = "Nét Việt";
-$tng_abbrev = "NetViet";
+$tng_title = "Hồng Bàng";
+$tng_abbrev = "HB";
 $tng_version = "1.0.0";
 $tng_copyright = "2013-" . date('Y');
 $tng_date = "15 July 2015";
